2b1b63e043d8f3c733a900d6e8238980e8fe6e8c
RaghavendraSR06 <raghu99459@gmail.com>
2021-03-16 23:20:22 +0530

    Update checker.cs

--- checker.cs
+++ checker.cs
@@ -4,11 +4,16 @@ namespace BatteryManagementSystem
     {
         static int Main()
         {
-            BatteryStatus.GermanLanguage = true;
-            BatteryStatus.CheckBatteryStatus(40, 84, 0.4f);
-            BatteryStatus.CheckBatteryStatus(-1, 20, 0.3f);
-            BatteryStatus.GermanLanguage = false;
-            BatteryStatus.CheckBatteryStatus(100, -20, 0.1f);
+            IReport report = new ConsoleLogger();
+            StatusMessages.GermanLanguage = true;
+            TestBatteryCondition consoleLogger = new TestBatteryCondition(report);
+            consoleLogger.CheckBatteryStatus(40, 84, 0.4f);
+            consoleLogger.CheckBatteryStatus(-1, 20, 0.3f);
+
+            StatusMessages.GermanLanguage = false;
+            report = new DatabaseLogger();
+            TestBatteryCondition dbLogger = new TestBatteryCondition(report);
+            dbLogger.CheckBatteryStatus(100, -20, 0.1f);
             return 0;
         }
     }
